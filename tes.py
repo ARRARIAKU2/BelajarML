1258775b84b36c726f471473ceb57e48712d1a54
ARRARIAKU2 <yerry_kecak@yahoo.com>
2022-03-26 20:07:00 +0700

    Belajar Database

---
+++ tes.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import datetime
+
+def calculatedaysbetweendates(date1, date2):
+    date1 = datetime.datetime.strptime(date1, "%Y-%m-%d")
+    date2 = datetime.datetime.strptime(date2, "%Y-%m-%d")
+    return abs((date2 - date1).days)
+
+print(calculatedaysbetweendates("2020-01-01", "2020-07-26"))
+
+# download video from twitter
+
+
+
